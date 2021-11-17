5cb7ad3f87a9feac7d231a2dff7631bd0ac07704
Dhiraj <jdhiraj543@gmail.com>
2021-11-17 12:55:16 +0530

    added follow/unfollow specific routes, and following array param

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ const morgan = require("morgan");
 const userRoute = require("./routes/users");
 const authRoute = require("./routes/auth");
 const postRoute = require("./routes/posts");
-
+const multer = require("multer");
+const path = require("path");
 dotenv.config();
 
 
@@ -17,11 +18,27 @@ app.use(express.json());
 app.use(helmet());
 app.use(morgan("common"));
 
+//uploading any file
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "public/images");
+    },
+    filename: (req, file, cb) => {
+        cb(null, req.body.name);
+    }
+})
+const upload = multer({ storage });
+app.post("/api/upload", upload.single("image"), (req, res) => {
+    try {
+        return res.status(200).json("File uploaded successfully")
+    } catch (err) {
+        console.log(err);
+    }
+});
 
 app.use("/api/user", userRoute);
-
 app.use("/api/auth", authRoute);
-
 app.use("/api/post", postRoute);
 
 mongoose.connect(
@@ -30,6 +47,8 @@ mongoose.connect(
         console.log("Connected to mongoDB");
     }
 );
+
+app.use("/images", express.static(path.join(__dirname, "public/images")));
 app.listen(8800, () => {
     console.log("backend Server is on port 8800");
 });
